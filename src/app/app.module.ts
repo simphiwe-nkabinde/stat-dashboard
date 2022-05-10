618e22239e1ca0134619f26e0641318ea56d6707
simphiwe <simphiwe.nkabinde@dadevs.co.za>
2022-05-10 05:06:19 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryCardComponent } from './components/summary-card/summary-card.component';
+import { TableComponent } from './components/table/table.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryCardComponent,
+    TableComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
